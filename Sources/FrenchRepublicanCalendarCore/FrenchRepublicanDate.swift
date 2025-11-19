fb9f0d32c9fdb496d34937fa5c3db1b18ece879c
Emil Pedersen <emil.pedersen@epitech.eu>
2025-11-19 16:42:26 +0100

    Fix maxSafeDate not auto-updating on options change

--- Sources/FrenchRepublicanCalendarCore/FrenchRepublicanDate.swift
+++ Sources/FrenchRepublicanCalendarCore/FrenchRepublicanDate.swift
@@ -18,8 +18,10 @@ public struct FrenchRepublicanDate: Hashable {
     
     /// The origin of the Republican Calendar, 1er Vendémiaire An 1 or 1792-09-22
     public static let origin = Date(timeIntervalSince1970: -5594191200)
-    /// The maximum safe date to convert, currently 15300-12-31
-    public static let maxSafeDate = FrenchRepublicanDateOptions.resolve(nil).variant.maxSafeDate
+    /// The maximum safe date to convert, depending on options: currently 2999-12-31 for Delambre, 15300-12-31 otherwise
+    public static var maxSafeDate: Date {
+        FrenchRepublicanDateOptions.resolve(nil).variant.maxSafeDate
+    }
     /// The safe range that is guaranteed to convert properly
     public static let safeRange = origin...maxSafeDate
     
